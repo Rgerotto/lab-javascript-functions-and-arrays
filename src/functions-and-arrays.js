09f5a52d061944738a257c5243f4e1c29646cd18
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-13 09:02:09 +0200

    labrafael.js

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,85 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+    if(a > b){
+      return a
+    }
+    else{
+      return b
+    }
+  }
+//console.log(maxOfTwoNumbers(3, 9));
+/*
 
 
+const countWord = words.slice()
+  console.log(countWord)
+  for(let i = 0; i < countWord.length; i++){
+    
+  }
 
+
+*/
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
-function findLongestWord() {}
-
-
+function findLongestWord(words) {
+  let singleWord = "";
+  console.log(words.length)
+  if(words.length === 0){
+    return null
+  }
+  else if(words.length === 1){
+    return words[0]
+  }
+  else{
+    let longestWord = words[0];
+    for(let i = 0; i < words.length; i++){
+      if(words[i].length > longestWord.length){
+        longestWord = words[i]
+      }
+    }
+    return longestWord
+  }
+ }
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArry) {
+if(numbersArry.length === 0){
+  return 0
+}
+else {
+  let sum = 0;
+  for(let j = 0; j < numbersArry.length; j++){
+    sum += numbersArry[j]
+  }
+  return sum
+}
+
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(numbersArray) {
+
+  if(numbersArray.length === 0){
+    return 0
+  }
+  else{
+    let sum = 0;
+    for(let i = 0; i < numbersArray.length; i++){
+      sum += numbersArray[i++];
+      return sum
+    }
+    let result = 0;
+    for(let i = 0; i < numbersArray.length; i++){
+      result += numbersArray[i]
+    }
+    return result
+  }
+  }
+
 
 
 
@@ -26,8 +87,14 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(numbersArray) {
+  if(numbersArray.length === 0){
+    return null
+  }
+  else{
+    return sumNumbers(numbersArray) /numbersArray.length
+  }
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
